3a82b7d8994d2d92717ab7f12331747f8cce8240
Jindřich Pergler <jindrich@pergler.eu>
2017-11-16 18:17:02 +0100

    Added init script for git

---
+++ GitScripts/Init/init-git.cmd
@@ -0,0 +1,6 @@
+
+git config --global core.autocrlf true
+
+git config --global alias.logg "log --oneline --decorate --graph --all"
+git config --global alias.logb "log --oneline --decorate --graph"
+
