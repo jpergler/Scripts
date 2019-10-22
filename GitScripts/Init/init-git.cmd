dbaa468f19110772487b8e59c1a4a3cb7064af9d
Jindřich Pergler <jindrich@pergler.eu>
2019-10-22 12:45:25 +0200

    Update init-git.cmd

--- GitScripts/Init/init-git.cmd
+++ GitScripts/Init/init-git.cmd
@@ -3,4 +3,5 @@ git config --global core.autocrlf true
 
 git config --global alias.logg "log --oneline --decorate --graph --all"
 git config --global alias.logb "log --oneline --decorate --graph"
-
+git config --global alias.fm "fetch origin master:master"
+git config --global alias.unmerge "reset --hard HEAD~1"
