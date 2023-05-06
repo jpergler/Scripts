d81555cff9b030c03d2b6bd013de908e8a81e0a4
Jindřich Pergler <jindrich@pergler.eu>
2023-05-06 15:30:26 +0200

    Update run-ravendb-windows.cmd

--- Docker/run-ravendb-windows.cmd
+++ Docker/run-ravendb-windows.cmd
@@ -1,1 +1,1 @@
-docker run --rm -d -p 8080:8080 -p 38888:38888 -v "/c/RavenDb/Data:/opt/RavenDB/Server/RavenData" --name ravendb-dev -e RAVEN_Setup_Mode=None -e RAVEN_License_Eula_Accepted=true -e RAVEN_Security_UnsecuredAccessAllowed=PrivateNetwork ravendb/ravendb:ubuntu-arm64v8-latest
+docker run --rm -d -p 8080:8080 -p 38888:38888 -v "/c/RavenDb/Data:/opt/RavenDB/Server/RavenData" --name ravendb-dev -e RAVEN_Setup_Mode=None -e RAVEN_License_Eula_Accepted=true -e RAVEN_Security_UnsecuredAccessAllowed=PrivateNetwork ravendb/ravendb:latest
