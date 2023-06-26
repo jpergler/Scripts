66801ddbfc543900a82b844d5af6c408026c93d6
Jindřich Pergler <jindrich@pergler.eu>
2023-06-26 22:04:10 +0200

    Update run-ravendb-m1mac.sh

--- Docker/run-ravendb-m1mac.sh
+++ Docker/run-ravendb-m1mac.sh
@@ -1,2 +1,2 @@
 # Run RavenDB in Docker on M1 Mac
-docker run --rm -d -p 8080:8080 -p 38888:38888 --name ravendb-dev -e RAVEN_Setup_Mode=None -e RAVEN_License_Eula_Accepted=true -e RAVEN_Security_UnsecuredAccessAllowed=PrivateNetwork ravendb/ravendb:ubuntu-arm64v8-latest
+docker run --rm -d -p 8080:8080 -p 38888:38888 -v ~/RavenData:/opt/RavenDB/Server/RavenData --name ravendb-dev -e RAVEN_Setup_Mode=None -e RAVEN_License_Eula_Accepted=true -e RAVEN_Security_UnsecuredAccessAllowed=PrivateNetwork ravendb/ravendb:ubuntu-arm64v8-latest
